ed6bff8a5f0d924edb6d8295344cd07652c00ed9
HelloDBA <hellodba@live.com>
2018-11-05 17:55:41 +0800

    增减加密解密相关内容

---
+++ SecurityUtility/DESEncrypt.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloDBA.Utilities.SecurityUtility
+{
+    public class DESEncrypt
+    {
+        #region ========加密========
+        /// <summary> 
+        /// 加密数据 
+        /// </summary> 
+        /// <param name="Text"></param> 
+        /// <param name="sKey"></param> 
+        /// <returns></returns> 
+        public static string Encrypt(string Text, string sKey)
+        {
+            DESCryptoServiceProvider des = new DESCryptoServiceProvider();
+            byte[] inputByteArray;
+            inputByteArray = Encoding.Default.GetBytes(Text);
+            des.Key = ASCIIEncoding.ASCII.GetBytes(CalMD5(sKey).Substring(0, 8));
+            des.IV = ASCIIEncoding.ASCII.GetBytes(CalMD5(sKey).Substring(0, 8));
+            System.IO.MemoryStream ms = new System.IO.MemoryStream();
+            CryptoStream cs = new CryptoStream(ms, des.CreateEncryptor(), CryptoStreamMode.Write);
+            cs.Write(inputByteArray, 0, inputByteArray.Length);
+            cs.FlushFinalBlock();
+            StringBuilder ret = new StringBuilder();
+            foreach (byte b in ms.ToArray())
+            {
+                ret.AppendFormat("{0:X2}", b);
+            }
+            return ret.ToString();
+        }
+
+        #endregion
+
+        #region ========解密========
+        /// <summary> 
+        /// 解密数据 
+        /// </summary> 
+        /// <param name="Text"></param> 
+        /// <param name="sKey"></param> 
+        /// <returns></returns> 
+        public static string Decrypt(string Text, string sKey)
+        {
+            if (string.IsNullOrEmpty(Text)) return "";
+            DESCryptoServiceProvider des = new DESCryptoServiceProvider();
+            int len;
+            len = Text.Length / 2;
+            byte[] inputByteArray = new byte[len];
+            int x, i;
+            for (x = 0; x < len; x++)
+            {
+                i = Convert.ToInt32(Text.Substring(x * 2, 2), 16);
+                inputByteArray[x] = (byte)i;
+            }
+            des.Key = ASCIIEncoding.ASCII.GetBytes(CalMD5(sKey).Substring(0, 8));
+            des.IV = ASCIIEncoding.ASCII.GetBytes(CalMD5(sKey).Substring(0, 8));
+            System.IO.MemoryStream ms = new System.IO.MemoryStream();
+            CryptoStream cs = new CryptoStream(ms, des.CreateDecryptor(), CryptoStreamMode.Write);
+            cs.Write(inputByteArray, 0, inputByteArray.Length);
+            cs.FlushFinalBlock();
+            return Encoding.Default.GetString(ms.ToArray());
+        }
+
+        #endregion
+
+        #region ========MD5========
+        public static string CalMD5(string str)
+        {
+            MD5 md5 = MD5.Create();
+            return BitConverter.ToString(md5.ComputeHash(Encoding.UTF8.GetBytes(str))).Replace("-", null);
+        }
+        #endregion
+    }
+}
